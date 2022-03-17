b3cb9ef05552f0a990d5cb84e20a99f5cf5c33d4
zpprsprk <zpprsprk@gmail.com>
2022-03-17 00:55:39 -0500

    Updates
    Including some ethanol unification and power core tweaks

--- overrides/kubejs/server_scripts/tags.js
+++ overrides/kubejs/server_scripts/tags.js
@@ -13,10 +13,4 @@ onEvent('item.tags', event => {
 
 	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
 	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
-	event.remove('forge:ingots/uranium', ['bigreactors:yellorium_ingot'])
-	event.remove('forge:dusts/uranium', ['bigreactors:yellorium_dust'])
-	event.remove('forge:ores/uranium', ['bigreactors:yellorite_ore'])
-	console.info("ALERTTHISISHERE" + event.get('forge:ingots/uranium'))
-//	event.get('forge:dusts/uranium').remove('bigreactors:yellorium_dust')
-//	event.get('forge:ores/uranium').remove('bigreactors:yellorite_ore')
 })
