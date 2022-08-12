46badbc822c37accd5491c830c7632dbfe1897ca
zpprsprk <zpprsprk@gmail.com>
2022-08-11 22:34:28 -0500

    Update to version 2.1.1

---
+++ overrides/kubejs/server_scripts/constants/general.js
@@ -0,0 +1,67 @@
+//priority: 1001
+
+const air = 'minecraft:air';
+
+// priority of which mod output should come from, if applicable
+const modPriorities = [
+    'emendatusenigmatica',
+    'minecraft',
+    'immersiveengineering',
+    'thermal',
+    'mekanism',
+    'kubejs',
+    'pneumaticcraft',
+    'create',
+    'createaddition',
+    'electrodynamics',
+    'cyclic',
+    'bno',
+    'blue_skies',
+    'occultism',
+    'industrialforegoing',
+    'tconstruct',
+    'ftbic',
+    'botania',
+    'quark',
+    'pedestals',
+    'refinedstorage',
+    'mapperbase',
+    'bloodmagic',
+    'eidolon',
+    'morevanillalib',
+    'titanium',
+    'mythicbotany',
+    'undergarden',
+    'byg',
+    'atum',
+    'betterendforge',
+    'miniutilities',
+    'chipped',
+    'chisel',
+    'voluminousenergy',
+    'assemblylinemachines',
+    'malum',
+    'material_elements'
+];
+
+const lootChests = ['lootr:lootr_chest', 'lootr:lootr_barrel', 'lootr:lootr_trapped_chest'];
+
+// Used for recipes/tags that use colors
+const colors = [
+    'cyan',
+    'purple',
+    'blue',
+    'brown',
+    'green',
+    'red',
+    'black',
+    'white',
+    'orange',
+    'magenta',
+    'light_blue',
+    'yellow',
+    'lime',
+    'pink',
+    'gray',
+    'light_gray'
+];
