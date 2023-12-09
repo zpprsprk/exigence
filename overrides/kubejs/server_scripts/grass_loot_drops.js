6671462f4a102fe6213ca8f1a1b938e2870e5638
zpprsprk <zpprsprk@gmail.com>
2023-12-09 00:24:44 -0600

    infinity catalyst recipe

--- overrides/kubejs/server_scripts/grass_loot_drops.js
+++
@@ -1,6 +0,0 @@
-LootJS.modifiers((event) => {
-    event
-        .addEntityLootModifier("minecraft:grass")
-        .randomChance(0.9) // 30% chance
-        .thenAdd("minecraft:pumpkin_seeds");
-});
