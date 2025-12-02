7c43dc12cb5b1d6c9a4e21fd34033929f74e44ee
zpprsprk <zpprsprk@gmail.com>
2025-12-02 00:07:17 -0600

    some Immersive Engineering recipes and other recipe fixes

---
+++ overrides/kubejs/server_scripts/jei.js
@@ -0,0 +1,6 @@
+console.info('Hello, World! This is the jei.js script')
+RecipeViewerEvents.addInformation('fluid', event => {
+    event.add('immersiveengineering:redstone_acid', [
+        'Redstone acid is obtained by putting witch water into a barrel on top of a redstone block. More redstone blocks speed up the process.'
+    ])
+})
